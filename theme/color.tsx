042356b1e6c8d342a48f30e2654dc8f46245f2dc
Morten Mc <Morten.M.Cramer@infomedia.dk>
2020-12-03 17:02:55 +0100

    Added a color file

---
+++ theme/color.tsx
@@ -0,0 +1,11 @@
+/** Plain old white */
+export const white = '#ffffff';
+
+/** Plain old black */
+export const black = '#000000';
+
+/** Darkest Gray */
+export const darkGray = '#222222';
+
+/** Lighter Gray */
+export const lighterGray = '#555555';
