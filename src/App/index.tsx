d80bdc53b986e282bd83e85ffdc4e78ffb47503e
Morten Mc <Morten.M.Cramer@infomedia.dk>
2020-12-03 22:02:56 +0100

    Message for my self: Make a startpage component.

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div style={{backgroundColor: "#222222", height: "100vh"}}>
       <Layout>
+        /** Siden skal være så clean som muligt. Lav en StartPage component, hvor du kan lave "Kortene" */
         <StartPage>
           <StartPageCards>Hej med dig</StartPageCards>
         </StartPage>
